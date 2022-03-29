8221958c06fc121047f2c53061eaf0e5e0a434a6
chandan mali <chandanmali21117@gmail.com>
2022-03-29 12:56:01 +0530

    bulb

---
+++ script.js
@@ -0,0 +1,9 @@
+function bulbOn() {
+  let bulb = document.querySelector(".off");
+  bulb.setAttribute("src", "./img/on bulb.jpg");
+}
+
+function bulbOff() {
+    let bulb = document.querySelector(".off");
+    bulb.setAttribute("src", "./img/off bulb.jpg");
+  }
